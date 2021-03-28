a59501d40b72323b079512a55b86aba873fa1dec
18469146268 <hnpxyj@126.com>
2021-03-28 22:12:35 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//vueCLI创建的
+// import {createApp} from 'vue'
+// import App from './App.vue'
+// import router from './router'
 
-createApp(App).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+//自己添加的
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
---
+++ src/views/home/Home.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>1</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+
+//若没有使用default导出，则使用{}导入
+import { getHomeMultidata, getHomeGoods } from "network/home";
+
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+  },
+  //保存请求过来的数据
+  data() {
+    return {
+      // result: null,
+      banners: [],
+      recommends: [],
+      goods: {
+        'pop': { page: 0, list: [] },
+        'new': { page: 0, list: [] },
+        'sell': { page: 0, list: [] },
+      },
+    };
+  },
+  //生命周期函数,在页面创建的时候请求数据
+  created() {
+    //1.请求多个数据,(异步操作)
+    this.getHomeMultidata();
+    //2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        // console.log(res);
+        // this.result = res;
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        /*
+        “...”是一种函数的解构，利用push可以传多个参数的特性
+        totalNum.push(.../num)——将数组num里面的元素逐一放进数组totalNum中去
+        */ 
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page+=1;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#home {
+  padding-top: 44px;
+}
+
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control {
+  /*让tab-control滚动到距离顶部还有44px时停下来（吸顶效果）*/
+  position: sticky;
+  top: 44px;
+}
+</style>
