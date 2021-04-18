2ac18538e1916f75ba3b2d4cb8a0cc349de1ea28
18469146268 <hnpxyj@126.com>
2021-04-18 18:34:51 +0800

    项目完成

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,13 +1,41 @@
 <template>
-  <h2>个人</h2>
+  <div>
+    <!--导航-->
+    <nav-bar class="nav-bar">
+      <div slot="center">个人中心</div>
+    </nav-bar>
+    <login />
+    <money />
+    <div class="line"></div>
+    <profile-list />
+  </div>
 </template>
 
 <script>
-export default{
-  name:'Profile'
-}
+import NavBar from "components/common/navbar/NavBar";
+import Login from "./childComps/Login";
+import Money from "./childComps/Money";
+import ProfileList from "./childComps/ProfileList";
+export default {
+  name: "Profile",
+  components: {
+    NavBar,
+    Login,
+    Money,
+    ProfileList,
+  },
+};
 </script>
 
 <style scoped>
-
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+  border-bottom: 1px solid #fff;
+}
+.line {
+  width: 100%;
+  height: 10px;
+  background-color: #eee;
+}
 </style>
---
+++ src/views/profile/childComps/ProfileList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="profile-list">
+    <div class="profile-list-item">
+      <img src="~assets/img/profile/message.svg"/>
+      <span>我的消息</span>
+    </div>
+    <div class="profile-list-item">
+      <img src="~assets/img/profile/pointer.svg"/>
+      <span>积分商城</span>
+    </div>
+    <div class="profile-list-item">
+      <img src="~assets/img/profile/vip.svg"/>
+      <span style="border-bottom: 0;">会员卡</span>
+    </div>
+    <div class="line"></div>
+    <div class="profile-list-item">
+      <img src="~assets/img/profile/cart.svg"/>
+      <span>我的购物车</span>
+    </div>
+    <div class="profile-list-item">
+      <img src="~assets/img/profile/shopping.svg"/>
+      <span>点赞购物APP</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "profileList"
+};
+</script>
+
+<style scoped>
+.profile-list {
+  width: 100%;
+}
+
+.profile-list-item {
+  line-height: 45px;
+  display: flex;
+  align-items: center;
+  height: 45px;
+  padding-left: 20px;
+}
+
+.profile-list-item img {
+  height: 25px;
+  width: 25px;
+  display: inline-block;
+  margin-right: 7px;
+}
+
+.profile-list-item span {
+  font-size: 16px;
+  display: inline-block;
+  width: 100%;
+  color: #333333;
+  border-bottom: 1px solid #dddddd;
+}
+
+.line {
+  width: 100%;
+  height: 10px;
+  background-color: #eeeeee;
+}
+</style>
