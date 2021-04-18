2889b99d75d2d27c87fdaf54a1d2ead9938f8d0a
18469146268 <hnpxyj@126.com>
2021-04-18 21:08:29 +0800

    修改接口

--- src/network/request.js
+++ src/network/request.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    // baseURL: 'http://123.207.32.32:8000',
-    baseURL:'http://152.136.185.210:7878/api/m5',
+    // baseURL: 'http://xxx.xxx.xx.xx:xxxx',
+    baseURL:'http://xxx.xxx.xxx.xxx:xxxx/xxx/xx',
     timeout: 5000
   })
 
